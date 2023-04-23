e6b0c5c853f2a24af286eb5d0e043e4340076cfd
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-23 13:43:24 +0100

    Added the swap function

--- sort.h
+++ sort.h
@@ -9,12 +9,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void print_array(const int*, unsigned int);
-void print_list(const listint_t*);
+void print_array(const int *, unsigned int);
+void print_list(const listint_t *);
 void bubble_sort(int *, unsigned int);
+void swap(int *, int *);
 #endif
