d7e420a0367e04ad208848f08e49535dceb74cc2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-23 08:28:40 +0100

    Project's header file

---
+++ sort.h
@@ -0,0 +1,19 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void print_array(const int*, unsigned int);
+void print_list(const listint_t*);
+#endif
